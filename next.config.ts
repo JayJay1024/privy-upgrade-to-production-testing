b8242e60923fb44277c1a7acebf1a28230c8887b
JayJay1024 <lygm1062@gmail.com>
2025-02-17 16:43:39 +0800

    update csp

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,18 @@ import type { NextConfig } from "next";
 
 const cspHeader = `
     default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' https://challenges.cloudflare.com;
     style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data:;
+    img-src 'self' blob: data: https://raw.githubusercontent.com;
     font-src 'self';
     object-src 'none';
     base-uri 'self';
     form-action 'self';
     frame-ancestors 'none';
     upgrade-insecure-requests;
+    child-src https://auth.privy.io https://verify.walletconnect.com https://verify.walletconnect.org https://privy.privy-upgrade-to-production-testing.vercel.app;
+    frame-src https://auth.privy.io https://verify.walletconnect.com https://verify.walletconnect.org https://challenges.cloudflare.com https://privy.privy-upgrade-to-production-testing.vercel.app;
+    connect-src 'self' https://auth.privy.io https://verify.walletconnect.com https://verify.walletconnect.org https://challenges.cloudflare.com wss://www.walletlink.org https://*.rpc.privy.systems https://explorer-api.walletconnect.com https://privy.privy-upgrade-to-production-testing.vercel.app;
 `;
 
 const nextConfig: NextConfig = {
